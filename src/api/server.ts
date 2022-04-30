8232bfb279915bef74018ba27ad6b43da9df154d
Matthew Darnell <matthew.darnell@protonmail.com>
2022-04-29 21:33:47 -0400

    Add rate limiter

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,8 @@
 
 import { createServer } from "http";
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+
 
 import { setupLastBlockRoute } from "./routes/last_block";
 import { setupBaseRoutes } from "./routes/base";
@@ -11,7 +13,19 @@ import { setupNftRoutes } from "./routes/nft";
 
 const express = require('express')
 const app = express()
+
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 10, // Limit each IP to 10 requests per `window`
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
 app.use(cors())
+app.use(limiter)
+
 const httpServer = createServer(app);
 
 console.log(`Initializing Express Server on Port ${parseInt(process.env.SERVERPORT) || 3000}`)
