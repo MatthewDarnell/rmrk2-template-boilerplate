9ee8ffa986cdfad0bcb1f9086bccc73d769bd1f4
Matthew Darnell <matthew.darnell@protonmail.com>
2022-06-10 16:56:46 -0400

    Properly wait for metadata fetcher to return before starting another loop Change some ipfs .env settings

--- src/services/metadata_fetcher.ts
+++ src/services/metadata_fetcher.ts
@@ -1,18 +1,25 @@
 import {addNftMetadata, getNftMetadataIpfsLink} from "../store/nft";
 
 
+
+const fetchMetadataLoop = async () => {
+    const nfts = await getNftMetadataIpfsLink(
+        process.env.IPFS_NUM_LINKS_FETCH ?
+            process.env.IPFS_NUM_LINKS_FETCH : 1
+    );
+    for(const nft of nfts) {
+        const { id, metadata } = nft
+        await addNftMetadata(id, 0, metadata)
+    }
+    return setTimeout(
+        fetchMetadataLoop,
+        process.env.IPFS_FETCH_TIMEOUT ?
+            parseInt(process.env.IPFS_FETCH_TIMEOUT) : 1000
+    );
+}
+
+
 export const startMetadataFetcher = async () => {
-    console.log('Starting Metadata Fetcher')
-    setInterval(async () => {
-            const nfts = await getNftMetadataIpfsLink(
-                                                        process.env.IPFS_NUM_LINKS_FETCH ?
-                                                                process.env.IPFS_NUM_LINKS_FETCH : 10
-            );
-            for(const nft of nfts) {
-                const { id, metadata } = nft
-                await addNftMetadata(id, 0, metadata)
-            }
-        },
-        process.env.IPFS_FETCH_TIMEOUT ? parseInt(process.env.IPFS_FETCH_TIMEOUT) : 750
-    )
+    console.log('Starting Metadata Fetcher');
+    return fetchMetadataLoop();
 }
