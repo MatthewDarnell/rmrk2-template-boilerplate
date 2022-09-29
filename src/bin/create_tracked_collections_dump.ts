8e1fa99bb8ac94260675c3ebfd6066adce09a58b
Matthew Darnell <matthew.darnell@protonmail.com>
2022-09-28 21:04:10 -0400

    Start expres server in create tracked collections dump, helps for server deployment

--- src/bin/create_tracked_collections_dump.ts
+++ src/bin/create_tracked_collections_dump.ts
@@ -17,6 +17,16 @@ import {
 import { getBaseById, getBaseChangesById, getBasePartsById} from "../store/base";
 import {getLastBlockScanned} from "../store/last_block";
 
+import tar from 'tar'
+const path = require('path')
+
+import { createServer } from "http";
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+
+const express = require('express')
+const app = express()
+
 
 const createDumpObject = async () => {
     try {
@@ -47,9 +57,9 @@ const createDumpObject = async () => {
             if(!coll) continue;
             if(!coll.length) continue;
 
-            
+
             collections[collection] = coll[0]
-            
+
             collections[collection]['changes'] = await getCollectionChangesById(collection) || []
 
             const nftsInCollection = await getNftsByCollection(collection)
@@ -73,9 +83,9 @@ const createDumpObject = async () => {
                             console.error(`Failed to Get base ${resource.base}!`)
                             continue
                         }
-                        
+
                         bases[base] = baseToGet[0]
-                        
+
                         bases[base]['changes'] = await getBaseChangesById(base) || []
                         let parts = await getBasePartsById(base) || []
 
@@ -86,7 +96,7 @@ const createDumpObject = async () => {
                             }
                         }
 
-                        
+
                         bases[base]['parts'] = parts
                     }
                 }
@@ -107,31 +117,110 @@ const createDumpObject = async () => {
             bases,
             lastBlock
         }
-
-
     } catch(error) {
         console.log(`Error Creating Dump File: ${error}`)
     }
 }
 
-createDumpObject().then(data => {
-    if (process.argv.length < 3) {
-        console.error("Missing Dump FileName!");
-        process.exit(0)
-    }
-    let appendLastBlock = false;
-    if(process.argv.length > 3) {
-        appendLastBlock = !!parseInt(process.argv[3])
-    }
-    const dumpFile = process.argv[2]
 
+if (process.argv.length < 4) {
+    console.error("Missing Dump FileName Or Seconds to Delay!\nUsage: ./app dump_filename seconds_to_delay_between_runs 1/0 (append lastBlock to filename)");
+    process.exit(0)
+}
+let appendLastBlock = false;
+let lastKnownBlock = 0;
+if(process.argv.length > 4) {
+    appendLastBlock = !!parseInt(process.argv[4])
+}
+const dumpFile = process.argv[2]
+const secondsToDelay = parseInt(process.argv[3]) || 3600
+
+
+createDumpObject().then(data => {
     const { lastBlock } = data
     console.log(`\nDone Reading DB. Got Last Block <${lastBlock}>`)
+    lastKnownBlock = lastBlock
     if(appendLastBlock) {
         fs.writeFileSync(`${dumpFile}-${lastBlock}`, JSON.stringify(data))
     } else {
         fs.writeFileSync(dumpFile, JSON.stringify(data))
+
+        tar.c( // or tar.create
+            {
+                gzip: true
+            },
+            [dumpFile]
+        ).pipe(fs.createWriteStream(`${dumpFile}.tar.gz`))
+        console.log(`Writing ${dumpFile}.tar.gz`)
     }
-    console.log('Done!')
-    process.exit(0)
+
+    return setInterval(() => {
+            createDumpObject().then(data => {
+                const { lastBlock } = data
+                console.log(`\nDone Reading DB. Got Last Block <${lastBlock}>`)
+                lastKnownBlock = lastBlock
+                if(appendLastBlock) {
+                    fs.writeFileSync(`${dumpFile}-${lastBlock}`, JSON.stringify(data))
+                    tar.c( // or tar.create
+                        {
+                            gzip: true
+                        },
+                        [`${dumpFile}-${lastBlock}`]
+                    ).pipe(fs.createWriteStream(`${dumpFile}-${lastBlock}.tar.gz`))
+                    console.log(`Writing ${dumpFile}-${lastBlock}.tar.gz`)
+                } else {
+                    fs.writeFileSync(dumpFile, JSON.stringify(data))
+                    tar.c( // or tar.create
+                        {
+                            gzip: true
+                        },
+                        [dumpFile]
+                    ).pipe(fs.createWriteStream(`${dumpFile}.tar.gz`))
+                    console.log(`Writing ${dumpFile}.tar.gz`)
+                }
+                console.log('Done!')
+                process.exit(0)
+            }).catch(console.error)
+        },
+        secondsToDelay * 1000
+    );
+
 }).catch(console.error)
+
+
+
+const maxCallsPerIpPerMin = process.env.MAXCALLSPERMIN ? parseInt(process.env.MAXCALLSPERMIN) : 20;
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: maxCallsPerIpPerMin, // Limit each IP to 10 requests per `window`
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
+app.use(cors())
+app.use(limiter)
+
+const httpServer = createServer(app);
+
+console.log(`Initializing Express Server on Port 3030`)
+
+app.get('/', (req, res) => {
+    try {
+        let file = dumpFile
+        if(appendLastBlock) {
+            file = `${dumpFile}-${lastKnownBlock}`
+        }
+        return res.sendFile(`${file}.tar.gz`, (error) => {
+            if(error) {
+                console.error(`Failed To Send: ${error}`)
+            } else {
+                console.log('sent!')
+            }
+        })
+    } catch(error) {
+        console.error(`Error: ${error}`)
+    }
+})
+
+httpServer.listen(  3030)
