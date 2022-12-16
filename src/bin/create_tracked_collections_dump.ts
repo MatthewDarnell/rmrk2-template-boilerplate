dbb36cb27a2328b0c478abff23ff517421ed0bbd
Matthew Darnell <matthew.darnell@protonmail.com>
2022-12-15 19:18:00 -0500

    Update tracked collections dumper app
    Delete old dumps
    Add latest block to dump file names
    Create latest block json file and do not create new dumps with smaller latest block

--- src/bin/create_tracked_collections_dump.ts
+++ src/bin/create_tracked_collections_dump.ts
@@ -27,9 +27,44 @@ import rateLimit from 'express-rate-limit'
 const express = require('express')
 const app = express()
 
+const lastBlockFile = process.env.LASTBLOCKFILE ? process.env.LASTBLOCKFILE : "lastblock.json";
+const maxDumpFilesToKeep = process.env.MAXDUMPSTOKEEP ? parseInt(process.env.MAXDUMPSTOKEEP) : 1;
+const dumpFile = process.argv[2]
+
+const getLatestBlockRead = () => {
+    try {
+        return parseInt(
+            JSON.parse(
+            fs.readFileSync(lastBlockFile)
+        )
+                ['lastBlock']
+        );
+    } catch(error) {
+        console.log(`Error Reading Last Block File.<${lastBlockFile}>. Setting to 0`);
+        return 0;
+    }
+}
+
+const updateLatestBlockRead = block => {
+    try {
+        const latestBlockInFile = getLatestBlockRead();
+        if(parseInt(block) > latestBlockInFile) {
+            fs.writeFileSync(lastBlockFile,
+                JSON.stringify({
+                    lastBlock: parseInt(block)
+                }
+            ));
+        }
+    } catch(error) {
+        console.error(`Failed To Update Latest Block Read to <${block}>!`);
+        console.error(error);
+    }
+}
+
 const createDumpObject = async () => {
     try {
-        console.log(`Starting RMRK Dumper. Connecting at <${process.env.PGUSER}@${process.env.PGHOST}:${process.env.PGPORT} -- ${process.env.DB}>`)
+        const latestBlockOfDumpFile = getLatestBlockRead();
+        console.log(`Starting RMRK Dumper.\n\tConnecting at <${process.env.PGUSER}@${process.env.PGHOST}:${process.env.PGPORT} -- ${process.env.DB}>\n\tLast Block File Stored at ${lastBlockFile} (${latestBlockOfDumpFile})`)
         open_database();
         const collectionsToGet = process.env.TRACKEDCOLLECTIONS? Array.from(process.env.TRACKEDCOLLECTIONS).join('').split(', ') : []
         if(collectionsToGet.length < 1) {
@@ -40,6 +75,16 @@ const createDumpObject = async () => {
         const bases = {}
         const collections = {}
         const nfts = {}
+
+
+        const lastBlock = (await getLastBlockScanned() || 1) - 1
+
+        if(latestBlockOfDumpFile >= lastBlock) {
+            return {
+                lastBlock,
+                updated: false
+            }
+        }
 
         for(const collection of collectionsToGet) {
             const coll = await getCollectionById(collection)
@@ -120,15 +165,15 @@ const createDumpObject = async () => {
             }
         }
 
-        const lastBlock = (await getLastBlockScanned() || 1) - 1
 
         await close_database()
-
+        updateLatestBlockRead(lastBlock);
         return {
             nfts,
             collections,
             bases,
-            lastBlock
+            lastBlock,
+            updated: true
         }
     } catch(error) {
         await close_database()
@@ -141,57 +186,81 @@ if (process.argv.length < 4) {
     console.error("Missing Dump FileName Or Seconds to Delay!\nUsage: ./app dump_filename seconds_to_delay_between_runs 1/0 (append lastBlock to filename)");
     process.exit(0)
 }
-let appendLastBlock = false;
+let appendLastBlock = true;
 let lastKnownBlock = 0;
 if(process.argv.length > 4) {
     appendLastBlock = !!parseInt(process.argv[4])
 }
-const dumpFile = process.argv[2]
+
 const secondsToDelay = parseInt(process.argv[3]) || 3600
 
 
-createDumpObject().then(data => {
-    const { lastBlock } = data
-    console.log(`\nDone Reading DB. Got Last Block <${lastBlock}>`)
-    lastKnownBlock = lastBlock
-    if(appendLastBlock) {
-        fs.writeFileSync(`${dumpFile}-${lastBlock}`, JSON.stringify(data))
-    } else {
-        fs.writeFileSync(dumpFile, JSON.stringify(data))
+const writeFile = data => {
+    try {
+        const folderOfDumpFiles = path.dirname(dumpFile)
+        const fileName = path.basename(dumpFile);
+        let oldFiles = fs.readdirSync(folderOfDumpFiles)
+            .filter(file => file.includes(fileName))
+        let compressedOldDumpFiles = oldFiles
+            .filter(file => file.includes('tar.gz'))
+        let rawDumpFiles = oldFiles.filter(file => compressedOldDumpFiles.indexOf(file) === -1)
 
-        tar.c( // or tar.create
-            {
-                gzip: true
-            },
-            [dumpFile]
-        ).pipe(fs.createWriteStream(`${dumpFile}.tar.gz`))
-        console.log(`Writing ${dumpFile}.tar.gz\nWaiting ${secondsToDelay} seconds.`)
+        if(rawDumpFiles.length > maxDumpFilesToKeep) {
+            const numToDelete = rawDumpFiles.length - maxDumpFilesToKeep;
+            for(let i = 0; i < numToDelete; i++) {
+                console.log(`Deleting Old Raw Dump File ${folderOfDumpFiles} / ${rawDumpFiles[i]}`)
+                fs.unlinkSync(`${folderOfDumpFiles}/${rawDumpFiles[i]}`)
+            }
+        }
+        if(compressedOldDumpFiles.length > maxDumpFilesToKeep) {
+            const numToDelete = compressedOldDumpFiles.length - maxDumpFilesToKeep;
+            for(let i = 0; i < numToDelete; i++) {
+                console.log(`Deleting Old Compressed Dump File ${compressedOldDumpFiles[i]}`)
+                fs.unlinkSync(`${folderOfDumpFiles}/${compressedOldDumpFiles[i]}`)
+            }
+        }
+
+
+        if(data['updated']) {
+            const oF = `${folderOfDumpFiles}/${data['lastBlock']}-${fileName}`;
+            console.log(`Writing New Dump ${oF}.tar.gz`);
+            if(appendLastBlock) {
+                fs.writeFileSync(oF, JSON.stringify(data))
+                tar.c( // or tar.create
+                    {
+                        gzip: true
+                    },
+                    [oF]
+                ).pipe(fs.createWriteStream(`${oF}.tar.gz`))
+
+            } else {
+                fs.writeFileSync(oF, JSON.stringify(data))
+                tar.c( // or tar.create
+                    {
+                        gzip: true
+                    },
+                    [dumpFile]
+                ).pipe(fs.createWriteStream(`${oF}.tar.gz`))
+            }
+            console.log(`Writing ${dumpFile}.tar.gz\nWaiting ${secondsToDelay} seconds.`)
+        }
+    } catch(error) {
+        console.error('Error Writing File');
+        console.error(error);
     }
+}
 
+createDumpObject().then(data => {
+    const { lastBlock, updated } = data
+    console.log(`\nDone Reading DB. Got Last Block <${lastBlock}>. Updated Dump ? ${updated}`);
+    lastKnownBlock = lastBlock
+    writeFile(data);
     return setInterval(() => {
             createDumpObject().then(data => {
-                const { lastBlock } = data
+                const { lastBlock, updated } = data
                 console.log(`\nDone Reading DB. Got Last Block <${lastBlock}>`)
                 lastKnownBlock = lastBlock
-                if(appendLastBlock) {
-                    fs.writeFileSync(`${dumpFile}-${lastBlock}`, JSON.stringify(data))
-                    tar.c( // or tar.create
-                        {
-                            gzip: true
-                        },
-                        [`${dumpFile}-${lastBlock}`]
-                    ).pipe(fs.createWriteStream(`${dumpFile}-${lastBlock}.tar.gz`))
-                    console.log(`Writing ${dumpFile}-${lastBlock}.tar.gz`)
-                } else {
-                    fs.writeFileSync(dumpFile, JSON.stringify(data))
-                    tar.c( // or tar.create
-                        {
-                            gzip: true
-                        },
-                        [dumpFile]
-                    ).pipe(fs.createWriteStream(`${dumpFile}.tar.gz`))
-                    console.log(`Writing ${dumpFile}.tar.gz`)
-                }
+                writeFile(data);
                 console.log('Done!')
             }).catch(console.error)
         },
@@ -222,7 +291,7 @@ app.get('/', (req, res) => {
     try {
         let file = dumpFile
         if(appendLastBlock) {
-            file = `${dumpFile}-${lastKnownBlock}`
+            file = `${lastKnownBlock}-${dumpFile}`
         }
         return res.sendFile(`${file}.tar.gz`, (error) => {
             if(error) {
